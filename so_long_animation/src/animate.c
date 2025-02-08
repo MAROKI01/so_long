e9f3a790684b6e4fde76c98cbada2f4621412d32
Noureddine Tahadout <ntahadou@c3r9p6.1337.ma>
2025-02-08 16:02:46 +0100

    Animation works well adding it for the coin and the enemy

--- so_long_animation/src/animate.c
+++ so_long_animation/src/animate.c
@@ -3,99 +3,129 @@
 #include "../include/libft.h"
 #include "../../include/so_long_utils.h"
 
-int animate_object(void *ptr, int object) {
-    t_data *data = (t_data *)ptr;
-    t_list *animation_node = data->sprites.coin.animations;
-
-    if (object == 'C')
-        animation_node = data->sprites.coin.animations;
-    else if (object == 'A')
-        animation_node = data->sprites.enemy.animations;
-
-    while (animation_node) {
-        t_animation *a = (t_animation *)animation_node->content;
-
-        if (a->_tmp_delay++ == a->delay) {
-            a->_tmp_delay = 0;
-            a->current_frame_num++;
-            a->current_frame_num %= ft_lstsize(a->frames);
-
-            t_img *img = (t_img *)ft_lstget_anim(a->frames, a->current_frame_num)->content;
-			// render_map(data);
-                for (int row = 0; row < data->height; row++)
-                  {
-                     for (int col = 0; col < data->width; col++)
-                         {
-                            if (data->grid[row][col] == object)
-                            {
-                                // render_tile(data, img, col * IMG_SIZE, row * IMG_SIZE);
-			                    render_player(data, img ,col * IMG_SIZE, row * IMG_SIZE);
-                            }
-                         }
-                  }
-			
-            mlx_put_image_to_window(img->win.mlx_ptr, img->win.win_ptr, data->background.ptr, 0, 0);
-        }
-        animation_node = animation_node->next;
-    }
-	return(0);
-}
-
-int update_animations(void *ptr) {
-    t_data *data = (t_data *)ptr;
-    t_list *animation_node = data->current_sprite.animations;
-
-    while (animation_node) {
-        t_animation *a = (t_animation *)animation_node->content;
-
-        if (a->_tmp_delay++ == a->delay) {
-            a->_tmp_delay = 0;
-            a->current_frame_num++;
-            a->current_frame_num %= ft_lstsize(a->frames);
-
-            t_img *img = (t_img *)ft_lstget_anim(a->frames, a->current_frame_num)->content;
-			render_map(data);
-			render_player(data, img ,data->player.p_x ,data->player.p_y);
-			
-            mlx_put_image_to_window(img->win.mlx_ptr, img->win.win_ptr, data->background.ptr, 0, 0);
-        }
-        animation_node = animation_node->next;
-    }
-        animate_object(data, 'C');
-        animate_object(data, 'A');
-	return(0);
-}
-
-
-t_sprite init_sprite(t_data *data, char *name, char *file_path, int frames, int width, int height)
+static t_list	*get_animation_list(t_data *data, int object)
 {
-    t_win tutorial = data->window;
-    
-    t_sprite s1 = new_sprite_anim(name, file_path, &tutorial);
+    if (object == 'C')
+        return (data->sprites.coin.animations);
+    else if (object == 'A')
+        return (data->sprites.enemy.animations);
+    return (NULL);
+}
+
+static void	render_object_frame(t_data *data, t_img *img, int object)
+{
+    int	row;
+    int	col;
+
+    row = 0;
+    while (row < data->height)
+    {
+        col = 0;
+        while (col < data->width)
+        {
+            if (data->grid[row][col] == object)
+                render_player(data, img, col * IMG_SIZE, row * IMG_SIZE);
+            col++;
+        }
+        row++;
+    }
+}
+
+static void	process_animation_frame(t_data *data, t_animation *a, int object)
+{
+    t_img	*img;
+
+    a->_tmp_delay = 0;
+    a->current_frame_num++;
+    a->current_frame_num %= ft_lstsize(a->frames);
+    img = (t_img *)ft_lstget_anim(a->frames, a->current_frame_num)->content;
+    render_object_frame(data, img, object);
+    mlx_put_image_to_window(img->win.mlx_ptr, img->win.win_ptr,
+        data->background.ptr, 0, 0);
+}
+
+static int	animate_object(void *ptr, int object)
+{
+    t_data	*data;
+    t_list	*animation_node;
+    t_animation	*a;
+
+    data = (t_data *)ptr;
+    animation_node = get_animation_list(data, object);
+    while (animation_node)
+    {
+        a = (t_animation *)animation_node->content;
+        if (a->_tmp_delay++ == a->delay)
+            process_animation_frame(data, a, object);
+        animation_node = animation_node->next;
+    }
+    return (0);
+}
+
+static void	process_player_animation_frame(t_data *data, t_animation *a)
+{
+    t_img	*img;
+
+    a->_tmp_delay = 0;
+    a->current_frame_num++;
+    a->current_frame_num %= ft_lstsize(a->frames);
+    img = (t_img *)ft_lstget_anim(a->frames, a->current_frame_num)->content;
+    render_map(data);
+    render_player(data, img, data->player.p_x, data->player.p_y);
+    mlx_put_image_to_window(img->win.mlx_ptr, img->win.win_ptr,
+        data->background.ptr, 0, 0);
+}
+
+static int	update_animations(void *ptr)
+{
+    t_data		*data;
+    t_list		*animation_node;
+    t_animation	*a;
+
+    data = (t_data *)ptr;
+    animation_node = data->current_sprite.animations;
+    while (animation_node)
+    {
+        a = (t_animation *)animation_node->content;
+        if (a->_tmp_delay++ == a->delay)
+            process_player_animation_frame(data, a);
+        animation_node = animation_node->next;
+    }
+    animate_object(data, 'C');
+    animate_object(data, 'A');
+    return (0);
+}
+
+static t_sprite init_sprite(t_data *data, char *name, char *file_path, int frames, int width, int height)
+{
+    t_win tutorial;
+    t_sprite s1;
+    sprite_slice slice1;
+
+    tutorial = data->window;
+    s1 = new_sprite_anim(name, file_path, &tutorial);
     if (!s1.sprite_img.ptr) {
         destroy_sprite_anim(s1);
         destroy_window_anim(tutorial);
         return (s1);
     }
-    sprite_slice slice1 = (sprite_slice){0, 0, width, height};
+    slice1 = (sprite_slice){0, 0, width, height};
     ft_lstadd_back(&s1.animations, ft_lstnew(slice_sprite_anim(data, s1, slice1, frames, 10000, PLAYER)));
     printf("Sprite %s [%d %d], loaded %d animations\n", s1.name, s1.width, s1.height, ft_lstsize(s1.animations));
     data->sprite = s1;
     return (s1);
 }
 
-int intialize_animations(t_data *data)
+static int intialize_animations(t_data *data)
 {
     data->player.direction = LEFT;
-        data->sprites.right = init_sprite(data, "right", "sprite_right.xpm",11,109,109);
-        data->sprites.left = init_sprite(data, "left", "sprite_left.xpm",11,109,109);
-        data->sprites.up = init_sprite(data, "up", "sprite_up.xpm",11,109,109);
-        data->sprites.down = init_sprite(data, "down", "sprite_down.xpm",11,109,109);
-        data->sprites.idle = init_sprite(data, "idle", "sprite_idle.xpm",25,109,109);
-        data->sprites.coin = init_sprite(data, "coin", "coin.xpm",9,32,32);
-        data->sprites.enemy = init_sprite(data, "enemy", "sprite_enemy.xpm",4,48,48);
-    
-    printf("data->direction %d\n", data->player.direction);
+    data->sprites.right = init_sprite(data, "right", "sprite_right.xpm",11,109,109);
+    data->sprites.left = init_sprite(data, "left", "sprite_left.xpm",11,109,109);
+    data->sprites.up = init_sprite(data, "up", "sprite_up.xpm",11,109,109);
+    data->sprites.down = init_sprite(data, "down", "sprite_down.xpm",11,109,109);
+    data->sprites.idle = init_sprite(data, "idle", "sprite_idle.xpm",25,109,109);
+    data->sprites.coin = init_sprite(data, "coin", "coin.xpm",9,32,32);
+    data->sprites.enemy = init_sprite(data, "enemy", "sprite_enemy2.xpm",12,32,32);
     return (0);
 }
 
@@ -104,7 +134,6 @@ int animate(t_data *data)
  	if (!data->window.win_ptr)
         return (2);
     keys_function(data);
-    // Update the animations; assume update_animations increments the frame and renders
     update_animations(data);
 	return (0);
 }
